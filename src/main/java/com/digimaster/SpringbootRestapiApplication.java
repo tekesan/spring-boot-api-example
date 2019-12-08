0b10f5dfa3dbc6521923faaac3941cc582b6641b
Bustomi Raharjo <bustomi.raharjo@tokopedia.com>
2019-12-09 00:07:54 +0700

    Initial commit

---
+++ src/main/java/com/digimaster/SpringbootRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.digimaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiApplication.class, args);
+	}
+
+}

bbfb2ffc5367da199c2fb0710e50b2528295f16c
Bustomi Raharjo <bustomi.raharjo@tokopedia.com>
2019-12-13 03:59:23 +0700

    dto object mapper

---
+++ src/main/java/com/digimaster/controller/UserModelMapperController.java
@@ -0,0 +1,42 @@
+package com.digimaster.controller;
+
+import com.digimaster.dtos.UserMmDto;
+import com.digimaster.entities.User;
+import com.digimaster.exceptions.UserNotFoundException;
+import com.digimaster.services.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Min;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/modelmapper/users")
+public class UserModelMapperController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@GetMapping("/{id}")
+	public UserMmDto getUserById(@PathVariable("id") @Min(1) Long id) throws UserNotFoundException {
+
+				 Optional<User> userOptional = userService.getUserById(id);
+				
+				if(!userOptional.isPresent()) {
+					throw new UserNotFoundException("user not found");
+				}
+				
+				User user = userOptional.get();
+			
+			UserMmDto userMmDto = modelMapper.map(user, UserMmDto.class);
+			return userMmDto;
+	
+	}
+}

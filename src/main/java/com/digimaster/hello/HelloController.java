d41612a46559a595a187379d9c5cdb3ecce823f0
Bustomi Raharjo <bustomi.raharjo@tokopedia.com>
2019-12-13 02:26:28 +0700

    internationalization

--- src/main/java/com/digimaster/hello/HelloController.java
+++ src/main/java/com/digimaster/hello/HelloController.java
@@ -1,10 +1,19 @@
 package com.digimaster.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 
     //Simple Method
     //URI - /helloworld
@@ -20,4 +29,17 @@ public class HelloController {
         return new User("Digi", "Master", "Jakarta");
     }
 
+    @GetMapping("/hello-int")
+    public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language", required=false)
+                                                  String locale) {
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+
+    }
+
+
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18NFormat2() {
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
+
+    }
 }
